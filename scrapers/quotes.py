1cd7f48639a0ed7665789916b80c8bc6da561769
Ivan Skorokhodov <iskorokhodov@gmail.com>
2017-01-06 16:40:19 +0300

    feat: add new ticker types

--- scrapers/quotes.py
+++ scrapers/quotes.py
@@ -3,18 +3,20 @@ import json
 import time
 import logging
 from datetime import datetime
+from itertools import islice
 
 import requests
 from peewee import *
 
-TICKERS = ['MMM', 'T', 'ADBE', 'AA', 'GOOG', 'AXP', 'AIG', 'AMT', 'AAPL', 'AMAT', 'BAC', 'BA', 'CA', 'CAT', 'CVX', 'CSCO', 'C', 'KO', 'GLW', 'DD', 'EMC', 'XOM', 'FSLR', 'GE', 'HPQ', 'HD', 'IBM', 'IP', 'INTC', 'JPM', 'JNJ', 'MCD', 'MRK', 'MSFT', 'PFE', 'PG', 'TRV', 'UTX', 'VZ', 'WMT', 'DIS', 'WFC', 'YHOO', 'YNDX']
+from .ticker import Ticker, TickerField
+
 INTERVAL_IDS = {60: 2, 300: 3, 600: 4, 900: 5, 1800: 6, 3600: 7}
 INTERVAL_TO_YEAR = {2: 2016, 3: 2015, 4: 2014, 5: 2013, 6: 2012, 7: 2011}
 BASE_URL = 'http://export.finam.ru/{ticker}.csv?market=25&em=20590&code={ticker}&apply=0&df={day}&mf={month}&yf={year}&p={interval_id}&f=lol&e=.csv&cn={ticker}&dtf=1&tmf=1&MSOR=1&mstimever=1&sep=1&sep2=1&datf=1&at=1'
 
 class Quote(Model):
-    ticker = CharField()
-    date = TimestampField()
+    ticker = TickerField()
+    date = TimestampField(utc=True, index=True)
     interval = DecimalField()
     open_price = DecimalField()
     low_price = DecimalField()
@@ -26,18 +28,27 @@ class Scraper:
     def __init__(self, db):
         self.db = db
 
-        with Using(db, [Quote]):
-            db.create_tables([Quote], safe=True)
+        with Using(self.db, [Quote]):
+            self.db.create_tables([Quote], safe=True)
 
     def scrape(self):
-        for ticker in TICKERS:
+        for ticker in Ticker:
             for interval in INTERVAL_IDS:
                 raw_quotes = self._load_quotes(ticker, interval)
-                self._transfer_quotes_to_db(raw_quotes)
+                quotes = self._extract_quotes(raw_quotes)
+                n = 100
+
+                with Using(self.db, [Quote], False):
+                    with self.db.atomic():
+                        while True:
+                            chunk = list(islice(quotes, n))
+
+                            if len(chunk) != 0: Quote.insert_many(chunk).execute()
+                            if len(chunk) < n: break
 
     def _load_quotes(self, ticker, interval):
         params = {
-            'ticker': ticker,
+            'ticker': ticker.name,
             'interval_id': INTERVAL_IDS[interval],
             'year': INTERVAL_TO_YEAR[INTERVAL_IDS[interval]],
             'month': 0,
@@ -49,19 +60,17 @@ class Scraper:
 
         return quotes
 
-    def _transfer_quotes_to_db(self, quotes):
+    def _extract_quotes(self, quotes):
         for quote in quotes.splitlines()[1:]:
-            with Using(self.db, [Quote], False):
-                with self.db.atomic():
-                    quote = quote.split(',')
+            quote = quote.split(',')
 
-                    Quote.insert(**{
-                        'ticker': quote[0],
-                        'date': time.mktime(datetime.strptime(quote[2] + quote[3], '%Y%m%d%H%M%S').timetuple()),
-                        'interval': int(quote[1]) * 60,
-                        'open_price': float(quote[4]),
-                        'high_price': float(quote[5]),
-                        'low_price': float(quote[6]),
-                        'close_price': float(quote[7]),
-                        'volume': int(quote[8])
-                    }).execute()
+            yield {
+                'ticker': Ticker[quote[0]],
+                'date': time.mktime(datetime.strptime(quote[2] + quote[3], '%Y%m%d%H%M%S').timetuple()),
+                'interval': int(quote[1]) * 60,
+                'open_price': float(quote[4]),
+                'high_price': float(quote[5]),
+                'low_price': float(quote[6]),
+                'close_price': float(quote[7]),
+                'volume': int(quote[8])
+            }
--- scrapers/ticker.py
+++ scrapers/ticker.py
@@ -9,18 +9,45 @@ class Ticker(Enum):
     WMT     = 2
     GE      = 3
     MSFT    = 4
-
-    @property
-    def fullname(self):
-        return _FULLNAMES[self]
-
-_FULLNAMES = {
-    Ticker.GOOG:    'Google',
-    Ticker.IBM:     'IBM',
-    Ticker.WMT:     'Walmart',
-    Ticker.GE:      'General Electric',
-    Ticker.MSFT:    'Microsoft'
-}
+    MMM     = 5
+    T       = 6
+    ADBE    = 7
+    AA      = 8
+    AXP     = 9
+    AIG     = 10
+    AMT     = 11
+    AAPL    = 12
+    AMAT    = 13
+    BAC     = 14
+    BA      = 15
+    CA      = 16
+    CAT     = 17
+    CVX     = 18
+    CSCO    = 19
+    C       = 20
+    KO      = 21
+    GLW     = 22
+    DD      = 23
+    EMC     = 24
+    XOM     = 25
+    FSLR    = 26
+    HPQ     = 27
+    HD      = 28
+    IP      = 29
+    INTC    = 30
+    JPM     = 31
+    JNJ     = 32
+    MCD     = 33
+    MRK     = 34
+    PFE     = 35
+    PG      = 36
+    TRV     = 37
+    UTX     = 38
+    VZ      = 39
+    DIS     = 40
+    WFC     = 41
+    YHOO    = 42
+    YND     = 43
 
 class TickerField(Field):
     db_field = 'smallint'
