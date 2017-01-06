2ed431f399d72c21af9a553beb44b5a3821c4a61
Paul Loyd <pavelko95@gmail.com>
2017-01-06 17:51:14 +0300

    feat(scrapers/tweets): add degradation

--- scrapers/tweets.py
+++ scrapers/tweets.py
@@ -1,7 +1,7 @@
 import logging
 import time
 import random
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from peewee import *
 from requests import Session
@@ -35,8 +35,8 @@ class Scraper:
         'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36',
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
         'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-        'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
-        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+        'Mozilla/5.0 (X11; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko'
     ]
 
     random.shuffle(_USER_AGENTS)
@@ -46,8 +46,12 @@ class Scraper:
         self.session = Session()
         self.ticker = ticker
         self.request_count = 0
+        self.fail_count = 0
         self.extracted = 0
-        self.started = 0
+        self.accepted = 0
+        self.startup = 0
+        self.upper_date = None
+        self.until = None
 
         with Using(db, [Tweet, TweetsContext]):
             db.create_tables([Tweet, TweetsContext], safe=True)
@@ -56,7 +60,7 @@ class Scraper:
                                                           defaults={'max_position': ''})
 
     def scrape(self):
-        self.started = time.time()
+        self.startup = time.time()
 
         if self.context.max_position:
             logging.info('Starting from %s', self.context.max_position)
@@ -66,38 +70,59 @@ class Scraper:
                 self._step()
 
     def _step(self):
-        json = self._fetch()
+        for i in range(len(self._USER_AGENTS), 0, -1):
+            json, ua = self._fetch()
+            self.request_count += 1
 
-        tweets = self._extract_tweets(json['items_html'])
+            html = json['items_html'].strip()
+            if html:
+                break
+
+            self.fail_count += 1
+
+            logging.warning('Empty document for %s', ua)
+
+            if i > 1:
+                logging.info('Retrying...')
+            else:
+                logging.info('Degradation...')
+                self._degrade()
+                return
+
+        tweets = list(self._extract_tweets(html))
         self.context.max_position = json['min_position']
 
-        with self.db.atomic():
-            count = Tweet.insert_many(tweets).execute()
-            self.context.save()
+        if tweets:
+            with self.db.atomic():
+                count = Tweet.insert_many(tweets).execute()
+                self.context.save()
 
     def _fetch(self):
+        until = (' until:' + self.until if self.until else '')
+
         params = {
             'f': 'realtime',
-            'q': self._QUERIES[self.ticker] + ' lang:en',
+            'q': self._QUERIES[self.ticker] + ' lang:en' + until,
             'src': 'typd',
             'max_position': self.context.max_position
         }
 
+        ua = self._USER_AGENTS[self.request_count % len(self._USER_AGENTS)]
+
         headers = {
-            'User-Agent': self._USER_AGENTS[self.request_count % len(self._USER_AGENTS)],
+            'User-Agent': ua,
             'X-Requested-With': "XMLHttpRequest"
         }
 
-        self.request_count += 1
-
         r = self.session.get('https://twitter.com/i/search/timeline', params=params, headers=headers)
 
-        return r.json()
+        return r.json(), ua
 
     def _extract_tweets(self, html):
         pq = PyQuery(html)
         oldest = 0
         extracted = 0
+        accepted = 0
 
         for tweet_html in pq('div.js-stream-tweet'):
             tweet = self._extract_tweet(tweet_html)
@@ -105,17 +130,19 @@ class Scraper:
             oldest = min(oldest or tweet['date'], tweet['date'])
             extracted += 1
 
-            yield tweet
+            if self._filter(tweet):
+                accepted += 1
+                yield tweet
 
         self.extracted += extracted
-        spent = (time.time() - self.started) / 3600
+        self.accepted += accepted
+        spent = (time.time() - self.startup) / 3600
 
-        MESSAGE = 'Extracted {total} (+{last}, {speed}t/h) up to {oldest} in {requests} requests'
-        logging.info(MESSAGE.format(total=self.extracted,
-                                    last=extracted,
-                                    speed=int(self.extracted / spent),
-                                    oldest=datetime.fromtimestamp(oldest),
-                                    requests=self.request_count))
+        logging.info('E: {} +{:2} {:5}/h    A: {} +{:2} {:5}/h    O: {}    R: {}    F: {}'.format(
+            self.extracted, extracted, int(self.extracted / spent),
+            self.accepted, accepted, int(self.accepted / spent),
+            datetime.fromtimestamp(oldest), self.request_count, self.fail_count
+        ))
 
     def _extract_tweet(self, html):
         pq = PyQuery(html)
@@ -131,3 +158,21 @@ class Scraper:
             'favorite_count': int(pq('span.ProfileTweet-action--favorite span.ProfileTweet-actionCount')
                                   .attr('data-tweet-stat-count').replace(',', ''))
         }
+
+    def _filter(self, tweet):
+        if tweet['retweet_count'] == 0 and tweet['favorite_count'] == 0:
+            return False
+
+        return not self.upper_date or tweet['date'] < self.upper_date
+
+    def _degrade(self):
+        self.context.max_position = ''
+
+        oldest = (Tweet
+            .select()
+            .where(Tweet.ticker == self.ticker)
+            .order_by(Tweet.date)
+            .get())
+
+        self.upper_date = oldest.date.timestamp()
+        self.until = (oldest.date + timedelta(days=1)).strftime('%Y-%m-%d')
