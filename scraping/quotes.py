7e2f49500bc2719d67afa5a9ee8868b3978967f2
Ivan Skorokhodov <iskorokhodov@gmail.com>
2017-01-29 12:42:01 +0300

    fix(scraping/quotes): scrape different quotes

--- scraping/quotes.py
+++ scraping/quotes.py
@@ -4,6 +4,8 @@ import time
 import logging
 from datetime import datetime
 from itertools import islice
+from collections import OrderedDict
+import time
 
 import requests
 
@@ -11,25 +13,38 @@ from base.database import db
 from base.schemas import Quote
 
 class Scraper:
-    _INTERVAL_IDS = {60: 2, 300: 3, 600: 4, 900: 5, 1800: 6, 3600: 7}
-    _INTERVAL_TO_YEAR = {2: 2016, 3: 2015, 4: 2014, 5: 2013, 6: 2012, 7: 2011}
-    _BASE_URL = 'http://export.finam.ru/{ticker}.csv?market=25&em=20590&code={ticker}&apply=0&df={day}&mf={month}&yf={year}&p={interval_id}&f=lol&e=.csv&cn={ticker}&dtf=1&tmf=1&MSOR=1&mstimever=1&sep=1&sep2=1&datf=1&at=1'
+    _INTERVAL_IDS = OrderedDict([(60, 2), (300, 3), (600, 4), (900, 5), (1800, 6), (3600, 7), (86400, 8)])
+    _INTERVAL_TO_YEAR = {2: 2016, 3: 2015, 4: 2014, 5: 2013, 6: 2012, 7: 2011, 8: 2010}
+    _BASE_URL = 'http://export.finam.ru/{ticker}.csv?market=25&em={ticker_id}&code={ticker}&apply=0&df={day}&mf={month}&yf={year}&p={interval_id}&f=lol&e=.csv&cn={ticker}&dtf=1&tmf=1&MSOR=1&mstimever=1&sep=1&sep2=1&datf=1&at=1'
 
-    _TICKERS = [
-        'AA', 'AAPL', 'ADBE', 'AIG', 'AMAT', 'AMT', 'AXP', 'BA', 'BAC', 'C', 'CA', 'CAT', 'CSCO',
-        'CVX', 'DD', 'DIS', 'EMC', 'FSLR', 'GE', 'GLW', 'GOOG', 'HD', 'HPQ', 'IBM', 'INTC', 'IP',
-        'JNJ', 'JPM', 'KO', 'MCD', 'MMM', 'MRK', 'MSFT', 'PFE', 'PG', 'T', 'TRV', 'UTX', 'VZ',
-        'WFC', 'WMT', 'XOM', 'YHOO', 'YND'
-    ]
+    _TICKERS = OrderedDict([
+        ("MMM", 18090), ("T", 19067), ("ADBE", 20563), ("AA", 17997), ("GOOG", 20590), ("AXP", 18009),
+        ("AIG", 19070), ("AMT", 20568), ("AAPL", 20569), ("AMAT", 20570), ("BAC", 18011), ("BA", 18010),
+        ("CA", 20576), ("CAT", 18026), ("CVX", 18037), ("CSCO", 20580), ("C", 18023), ("KO", 18076),
+        ("GLW", 20582), ("DD", 18039), ("EMC", 20585), ("XOM", 18149), ("FSLR", 20586), ("GE", 18055),
+        ("GS", 472568), ("HPQ", 18068), ("HD", 18063), ("IBM", 18069), ("IP", 22141), ("INTC", 19069),
+        ("JPM", 18074), ("JNJ", 18073), ("MCD", 18080), ("MRK", 18094), ("MSFT", 19068), ("PFE", 18106),
+        ("PG", 18107), ("TRV", 22139), ("UTX", 18134), ("VZ", 18137), ("WMT", 18146), ("DIS", 18041),
+        ("WFC", 22138), ("YHOO", 19075), ("YNDX", 81151)
+    ])
 
     def __init__(self):
         db.create_tables([Quote], safe=True)
 
     def scrape(self):
-        for ticker in self._TICKERS:
+        for ticker in self._TICKERS.keys():
             for interval in self._INTERVAL_IDS:
-                raw_quotes = self._load_quotes(ticker, interval)
-                quotes = self._extract_quotes(raw_quotes, interval)
+                response = self._load_quotes(ticker, interval)
+
+                if 'Дождитесь' in response:
+                    time.sleep(3)
+                    response = self._load_quotes(ticker, interval)
+
+                if 'слишком большой' in response:
+                    print('Skipping {interval} for {ticker}'.format(interval, ticker))
+                    continue # Finam does not have quotes for this period
+
+                quotes = self._extract_quotes(response, interval)
                 n = 100
 
                 with db.atomic():
@@ -43,9 +58,11 @@ class Scraper:
 
     def _load_quotes(self, ticker, interval):
         interval_id = self._INTERVAL_IDS[interval]
+        ticker_id = self._TICKERS[ticker]
 
         params = {
             'ticker': ticker,
+            'ticker_id': ticker_id,
             'interval_id': interval_id,
             'year': self._INTERVAL_TO_YEAR[interval_id],
             'month': 0,
@@ -53,9 +70,9 @@ class Scraper:
         }
 
         logging.info('Getting {ticker} ({interval_id}) since {year}-{month:02}-{day:02}'.format(**params))
-        quotes = requests.get(self._BASE_URL.format(**params)).text
+        response = requests.get(self._BASE_URL.format(**params))
 
-        return quotes
+        return response.text
 
     def _extract_quotes(self, quotes, interval):
         for quote in quotes.splitlines()[1:]:
